0d6de577d96e1ebc4c3008ab13a6c5a2b309bdf7
vishnupsatish <vishnupavan.satish@gmail.com>
2021-01-13 14:28:48 -0500

    Add classes page and finish dashboard

--- application/routes/teacher.py
+++ application/routes/teacher.py
@@ -11,6 +11,13 @@ def teacher_login():
     form = LoginForm()
     return render_template('teacher/general/login.html', form=form)
 
+
 @app.route('/dashboard')
 def teacher_dashboard():
     return render_template('teacher/general/dashboard.html')
+
+
+@app.route('/class/1/home')
+def teacher_class():
+    return render_template('teacher/classes/home.html')
+
